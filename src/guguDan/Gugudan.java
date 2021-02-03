c61b4cc7181bd47d5afecb004697d163c983637a
SAMSUNG <SAMSUNG@LAPTOP-6AJ0SCTP>
2021-02-03 18:56:11 +0900

    3단 출력 추가

--- src/guguDan/Gugudan.java
+++ src/guguDan/Gugudan.java
@@ -3,8 +3,15 @@ package guguDan;
 public class Gugudan {
 	public static void main(String[] args) {
 		//2단
+		System.out.println("2단");
 		for(int i=1; i<=9; i++) {
 			System.out.println("2 * " + i + " = " + 2*i);
+		}
+		
+		//3단
+		System.out.println("3단");
+		for(int i=1; i<=9; i++) {
+			System.out.println("3 * " + i + " = " + 3*i);
 		}
 	}
 }
