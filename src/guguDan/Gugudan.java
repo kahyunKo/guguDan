c2804a5311a37b3f436dafbfdfb34aec40dd24fb
SAMSUNG <SAMSUNG@LAPTOP-6AJ0SCTP>
2021-02-03 18:52:59 +0900

    2단 출력

---
+++ src/guguDan/Gugudan.java
@@ -0,0 +1,10 @@
+package guguDan;
+
+public class Gugudan {
+	public static void main(String[] args) {
+		//2´Ü
+		for(int i=1; i<=9; i++) {
+			System.out.println("2 * " + i + " = " + 2*i);
+		}
+	}
+}
