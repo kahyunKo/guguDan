973a827405bb80219dad0e93c5fbd854a4487a8b
SAMSUNG <SAMSUNG@LAPTOP-6AJ0SCTP>
2021-02-03 19:57:30 +0900

    추가 요구사항 수정

--- src/guguDan/ArrayGugudan.java
+++ src/guguDan/ArrayGugudan.java
@@ -1,20 +1,19 @@
 package guguDan;
-import java.util.Scanner;
 
 public class ArrayGugudan {
 	public static void main(String[] args) {
 		
-		System.out.println("출력할 단을 입력하세요");
-		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
+		for( int j = 2; j < 10; j++ ) {
+			int[] result  = new int[9];
+			for ( int i = 0; i < result.length; i++ ) {
+				result[i] =  j * ( i + 1 );
+			}
 		
-		int[] result  = new int[9];
-		for ( int i = 0; i < result.length; i++ ) {
-			result[i] =  number*(i+1);
-		}
-		
-		for(int i = 0; i <= result.length; i++ ) {
-			System.out.println(result[i]);
+			System.out.println(j+"단 출력");
+			
+			for(int i = 0; i < result.length; i++ ) {
+				System.out.println(result[i]);
+			}
 		}
 	}
 }
