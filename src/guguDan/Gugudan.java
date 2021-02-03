296f40163a8dbe53b3e626da202910d9546a8080
SAMSUNG <SAMSUNG@LAPTOP-6AJ0SCTP>
2021-02-03 19:11:09 +0900

    사용자 값 입력

--- src/guguDan/Gugudan.java
+++ src/guguDan/Gugudan.java
@@ -1,17 +1,21 @@
 package guguDan;
+import java.util.Scanner;
 
 public class Gugudan {
 	public static void main(String[] args) {
-		//2단
-		System.out.println("2단");
-		for(int i=1; i<=9; i++) {
-			System.out.println("2 * " + i + " = " + 2*i);
-		}
+		System.out.println("구구단 중 출력할 단은?");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		System.out.println("number : " + number);
 		
-		//3단
-		System.out.println("3단");
-		for(int i=1; i<=9; i++) {
-			System.out.println("3 * " + i + " = " + 3*i);
+		if( number < 2 ) {
+			System.out.println("2 이상, 9 이하의 값만 입력할 수 있습니다.");
+		} else if ( number > 9 ) {
+			System.out.println("2 이상, 9 이하의 값만 입력할 수 있습니다.");
+		} else {
+			for( int i = 1; i < 9; i++ ) {
+				System.out.println(number + " * " + i + " = " + number*i );
+			}
 		}
 	}
 }
