16dd30dbfd2333db94d33bb7bcf872914d4a0dd7
SAMSUNG <SAMSUNG@LAPTOP-6AJ0SCTP>
2021-02-04 17:58:43 +0900

    calculate method 생성

--- src/guguDan/MethodGugudan.java
+++ src/guguDan/MethodGugudan.java
@@ -1,28 +1,30 @@
 package guguDan;
 
-public class MethodGugudan {
-	public static void main(String[] args) {
-		
+public class MethodGugudan { 
+	public static int[] calculate(int times) {
 		int[] result = new int[9];
 		for( int i = 0; i < result.length; i++ ) {
-			result[i] = 2 * (i+1);
+			result[i] = times * (i+1);
 		}
+		return result;
+	}   // int[] 반환받을 배열의 데이터 타입
+	
+	public static void main(String[] args) {
+		
+		int[] result = calculate(2);
+		
 		for( int i = 0; i < result.length; i++ ) {
 			System.out.println(result[i]);
 		}
 		
-		int[] times3 = new int[9];
-		for( int i = 0; i < times3.length; i++ ) {
-			times3[i] = 3 * (i+1);
-		}
+		int[] times3 = calculate(3);
+		
 		for( int i = 0; i < times3.length; i++ ) {
 			System.out.println(times3[i]);
 		}
 		
-		int[] times4 = new int[9];
-		for( int i = 0; i < times4.length; i++ ) {
-			times4[i] = 4 * (i+1);
-		}
+		int[] times4 = calculate(4);
+		
 		for( int i = 0; i < times4.length; i++ ) {
 			System.out.println(times4[i]);
 		}
