cf989ae8015a12580627f38b9025aedf969448c8
SAMSUNG <SAMSUNG@LAPTOP-6AJ0SCTP>
2021-02-04 18:27:36 +0900

    class gugudan

---
+++ src/guguDan/GugudanMain.java
@@ -0,0 +1,10 @@
+package guguDan;
+
+public class GugudanMain {
+	public static void main(String[] args) {
+		for(int i = 2; i < 10; i++ ) {
+			int[] result = ClassGugudan.calculate(i);
+			ClassGugudan.print(result);
+		}
+	}
+}
